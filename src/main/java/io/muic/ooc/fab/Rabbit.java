a0f490c65f824bb5ef7881e4cf64f11ae6f12805
mickeyverve <88787405+mickeyverve@users.noreply.github.com>
2022-02-07 21:19:59 +0700

    Refractoring and rearranging the code

--- src/main/java/io/muic/ooc/fab/Rabbit.java
+++ src/main/java/io/muic/ooc/fab/Rabbit.java
@@ -4,20 +4,6 @@ import java.util.List;
 import java.util.Random;
 
 public class Rabbit extends Animal {
-    // Characteristics shared by all rabbits (class variables).
-
-    // The age at which a rabbit can start to breed.
-    private static final int BREEDING_AGE = 5;
-    // The age to which a rabbit can live.
-    private static final int MAX_AGE = 40;
-    // The likelihood of a rabbit breeding.
-    private static final double BREEDING_PROBABILITY = 0.12;
-    // The maximum number of births.
-    private static final int MAX_LITTER_SIZE = 4;
-    // Random generator
-    private static final Random RANDOM = new Random();
-
-
     /**
      * Create a new rabbit. A rabbit may be created with age zero (a new born)
      * or with a random age.
@@ -32,24 +18,34 @@ public class Rabbit extends Animal {
 
     @Override
     protected double getBreedingProbability() {
-        return BREEDING_PROBABILITY;
+        return 0.12;
     }
 
     @Override
     protected int getMaxLitterSize() {
-        return MAX_LITTER_SIZE;
+        return 4;
+    }
+
+    @Override
+    protected int getMaxAge() {
+        return 40;
+    }
+
+    @Override
+    protected int getBreedingAge() {
+        return 5;
     }
 
     /**
      * This is what the rabbit does most of the time - it runs around. Sometimes
      * it will breed or die of old age.
      *
-     * @param newRabbits A list to return newly born rabbits.
+     * @param newAnimals A list to return newly born rabbits.
      */
-    public void act(List<Animal> newRabbits) {
+    public void act(List<Animal> newAnimals) {
         incrementAge();
         if (isAlive()) {
-            giveBirth(newRabbits);
+            giveBirth(newAnimals);
             // Try to move into a free location.
             Location newLocation = getField().freeAdjacentLocation(getLocation());
             if (newLocation != null) {
@@ -59,16 +55,6 @@ public class Rabbit extends Animal {
                 setDead();
             }
         }
-    }
-
-    @Override
-    protected int getMaxAge() {
-        return MAX_AGE;
-    }
-
-    @Override
-    protected int getBreedingAge() {
-        return BREEDING_AGE;
     }
 
     /**
